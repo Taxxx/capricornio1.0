9422dd447f3e6ba5916b025b9392f5899ec178e9
Alexs14 <jpmtnt@gmail.com>
2015-06-10 10:42:14 -0600

    notificacion detramite otra vez

--- src/umsa/capricornio/domain/RestriccionesFechasData.java
+++ src/umsa/capricornio/domain/RestriccionesFechasData.java
@@ -17,6 +17,7 @@ public class RestriccionesFechasData {
     public String nro_resolucion_inicio;
     public String estado;
     public String dias_restantes;
+    public String m_actividad;
 
     public RestriccionesFechasData() {
         this.fecha_inicio = "";
@@ -25,15 +26,17 @@ public class RestriccionesFechasData {
         this.nro_resolucion_inicio = "";
         this.estado = "";
         this.dias_restantes = "";
+        this.m_actividad="";
     }
 
-    public RestriccionesFechasData(String fecha_inicio, String fecha_final, String nro_solicitud_compra, String nro_resolucion_inicio, String estado, String dias_restantes) {
+    public RestriccionesFechasData(String fecha_inicio, String fecha_final, String nro_solicitud_compra, String nro_resolucion_inicio, String estado, String dias_restantes,String m_actividad) {
         this.fecha_inicio = fecha_inicio;
         this.fecha_final = fecha_final;
         this.nro_solicitud_compra = nro_solicitud_compra;
         this.nro_resolucion_inicio = nro_resolucion_inicio;
         this.estado = estado;
         this.dias_restantes = dias_restantes;
+        this.m_actividad=m_actividad;
     }
     
 }
