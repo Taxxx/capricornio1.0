0574bea9ef160aa8921c867f786857084e549381
Alexs14 <jpmtnt@gmail.com>
2015-06-16 09:09:39 -0600

    cambio del limite de dias restringiendo a los anpe

--- src/umsa/capricornio/gui/transacciones/Adquisiciones/tramites/FrmAdquisiciones.java
+++ src/umsa/capricornio/gui/transacciones/Adquisiciones/tramites/FrmAdquisiciones.java
@@ -303,7 +303,9 @@ public class FrmAdquisiciones extends javax.swing.JInternalFrame {
         int cod_trans_nro = Integer.parseInt(TblTransaccionBandeja.getValueAt(fila, 0).toString());        
         //menu.AbrirOtroFrame(this, new FrmOrdenesDetalle(menu, this,cod_almacen, cod_trans_nro,cod_rol,  TblTransaccionBandeja.getValueAt(fila, 4).toString(), gestion, Integer.parseInt(TblTransaccionBandeja.getValueAt(fila, 2).toString()),TblTransaccionBandeja.getValueAt(fila, 1).toString(),TblTransaccionBandeja.getValueAt(fila, 5).toString(),TblTransaccionBandeja.getValueAt(fila, 6).toString(),TblTransaccionBandeja.getValueAt(fila, 7).toString()));
         verificadias(menu,cod_almacen, cod_trans_nro,cod_rol,  TblTransaccionBandeja.getValueAt(fila, 4).toString(), gestion, Integer.parseInt(TblTransaccionBandeja.getValueAt(fila, 2).toString()),TblTransaccionBandeja.getValueAt(fila, 1).toString(),TblTransaccionBandeja.getValueAt(fila, 5).toString(),TblTransaccionBandeja.getValueAt(fila, 6).toString(),TblTransaccionBandeja.getValueAt(fila, 7).toString(),TblTransaccionBandeja.getValueAt(fila, 3).toString(),TblTransaccionBandeja.getValueAt(fila, 9).toString(),TblTransaccionBandeja.getValueAt(fila, 10).toString(),TblTransaccionBandeja.getValueAt(fila, 11).toString());
-        if(pasa==-1)
+        String cuantia=TblTransaccionBandeja.getValueAt(fila, 9).toString();
+        System.out.println("la cuantia: "+cuantia);
+        if(pasa==-1 && !cuantia.equals("ANPE"))
         {
             System.out.println("no tiene que abrir");
             pasa=0;
